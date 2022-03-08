d87dc4341389537d1290cdcdb47da0bf755c38ba
tduncan14 <nbatre1@yahoo.com>
2022-03-08 13:34:50 -0800

    keep moving forward

--- js/script.js
+++ js/script.js
@@ -12,3 +12,16 @@ document.querySelector('#search-btn').onclick = () => {
     console.log('hello world')
     searchForm.classList.toggle('active');
 }
+
+
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () => {
+
+    cart.classList.toggle('active')
+}
+
+
+
+let loginForm = document.querySelector('.login-form');

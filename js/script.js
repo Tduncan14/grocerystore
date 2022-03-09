4de3815cebedf0230300efda944647b4eb4954f7
tduncan14 <nbatre1@yahoo.com>
2022-03-08 16:40:11 -0800

    keep moving forward

--- js/script.js
+++ js/script.js
@@ -11,6 +11,9 @@ document.querySelector('#search-btn').onclick = () => {
     
     console.log('hello world')
     searchForm.classList.toggle('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 
@@ -20,6 +23,9 @@ let cart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () => {
 
     cart.classList.toggle('active')
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active')
+    loginForm.classList.remove('active');
 }
 
 
@@ -30,6 +36,10 @@ let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () => {
 
     loginForm.classList.toggle('active');
+    cart.classList.remove('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active')
+  
 }
 
 
@@ -41,6 +51,9 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick  = () => {
 
     navbar.classList.toggle('active');
+    cart.classList.remove('active');
+    searchForm.classList.remove('active')
+    loginForm.classList.remove('active');
 
 }
 
